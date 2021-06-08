a5305332efc3a3996f7d49c78f2f8fd2bf047b70
Elahesmailpour <eli_es87@yahoo.com>
2021-06-08 13:44:58 +0200

    basic1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {SeverComponent} from "./server/server.component"
+import {SeverComponent} from "./server/server.component";
+import { ServersComponent } from './servers/servers.component'
 @NgModule({
   declarations: [
     AppComponent,
-    SeverComponent
+    SeverComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
